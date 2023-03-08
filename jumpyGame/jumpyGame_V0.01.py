bdbec3a0b9bec76e122084172db734dcfb90b898
Varier2006 <matus.capo11@gmail.com>
2023-03-08 18:16:31 +0100

    fixed movement

--- jumpyGame/jumpyGame_V0.01.py
+++ jumpyGame/jumpyGame_V0.01.py
@@ -5,80 +5,7 @@ from tkinter import *
 # cez slider nech sa urcuje rychlost a ine veci ako velkost dosiek a ich frekvencia
 # Gravtacia a player nech je na neho relativita robit
 # Centered na playera aj s cords, nech je on 0,0 vzdy vlastne takze no nevem
-"""
-# imports every file form tkinter and tkinter.ttk
-from tkinter import *
-from tkinter.ttk import *
 
-class GFG:
-	def __init__(self, master = None):
-		self.master = master
-		
-		# to take care movement in x direction
-		self.x = 1
-		# to take care movement in y direction
-		self.y = 0
-
-		# canvas object to create shape
-		self.canvas = Canvas(master)
-		# creating rectangle
-		self.rectangle = self.canvas.create_rectangle(
-						5, 5, 25, 25, fill = "black")
-		self.canvas.pack()
-
-		# calling class's movement method to
-		# move the rectangle
-		self.movement()
-	
-	def movement(self):
-
-		# This is where the move() method is called
-		# This moves the rectangle to x, y coordinates
-		self.canvas.move(self.rectangle, self.x, self.y)
-
-		self.canvas.after(100, self.movement)
-	
-	# for motion in negative x direction
-	def left(self, event):
-		print(event.keysym)
-		self.x = -5
-		self.y = 0
-	
-	# for motion in positive x direction
-	def right(self, event):
-		print(event.keysym)
-		self.x = 5
-		self.y = 0
-	
-	# for motion in positive y direction
-	def up(self, event):
-		print(event.keysym)
-		self.x = 0
-		self.y = -5
-	
-	# for motion in negative y direction
-	def down(self, event):
-		print(event.keysym)
-		self.x = 0
-		self.y = 5
-
-if __name__ == "__main__":
-
-	# object of class Tk, responsible for creating
-	# a tkinter toplevel window
-	master = Tk()
-	gfg = GFG(master)
-
-	# This will bind arrow keys to the tkinter
-	# toplevel which will navigate the image or drawing
-	master.bind("<KeyPress-Left>", lambda e: gfg.left(e))
-	master.bind("<KeyPress-Right>", lambda e: gfg.right(e))
-	master.bind("<KeyPress-Up>", lambda e: gfg.up(e))
-	master.bind("<KeyPress-Down>", lambda e: gfg.down(e))
-	
-	# Infinite loop breaks only by interrupt
-	mainloop()
-"""
 root = Tk()
 root.geometry("1280x720")
 root.title("Hmm")
@@ -86,10 +13,10 @@ canvas = Canvas(root, width="1280", height="720")
 canvas.pack()
 
 dosky = []
-
+key = ""
 
 class Player:
-    def __init__(self):
+    def __init__(self, master=None):
         self.x = 0
         self.y = 0
 
@@ -119,6 +46,22 @@ class Player:
         self.y = 5
         self.movement()
 
+    def keypressed(self):
+        global key
+        if key == " ":
+            print("jum")
+            jumped()
+        elif key == "w":
+            self.up()
+        elif key == "a":
+            self.left()
+        elif key == "s":
+            self.down()
+        elif key == "d":
+            self.right()
+        else:
+            print(key)
+
 
 class Doska:
     def __init__(self, x1, y1, x2, y2):
@@ -128,6 +71,7 @@ class Doska:
         self.y2 = y2
 
 
+player = Player()
 doska0 = Doska(0, 0, 0, 0)
 dosky.append(doska0)
 doska1 = Doska(0, 0, 0, 0)
@@ -164,20 +108,12 @@ def jumped():
         canvas.update()
 
 
-def keypressed(arg):
-    if arg.char == " ":
-        print("jum")
-        jumped()
-    elif arg.char == "w":
-        Player.up()
-    elif arg.char == "a":
-        Player.left()
-    elif arg.char == "s":
-        Player.down()
-    elif arg.char == "d":
-        Player.right()
-    else:
-        print()
+def keypress(arg):
+    global key
+    pressed = True
+    key = arg.char
+    player.keypressed()
 
-root.bind("<Key>", keypressed)
+
+root.bind("<Key>", keypress)
 canvas.mainloop()
