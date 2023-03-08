6e84439041cd3251485364e1b23f59e7edc9c8d2
Varier2006 <matus.capo11@gmail.com>
2023-03-08 22:41:37 +0100

    Gravity implemented

--- jumpyGame/jumpyGame_V0.01.py
+++ jumpyGame/jumpyGame_V0.01.py
@@ -1,10 +1,11 @@
-from tkinter import *
-
 # vsetko sa hybe len player je vzdy centered, jeden global y podla ktoreho sa vsetko sprava iba s offsetom
 # y nech je len jedno ako main pozicia a to druhe nech je len + daco aby to definovalo hrubku dosky
 # cez slider nech sa urcuje rychlost a ine veci ako velkost dosiek a ich frekvencia
 # Gravtacia a player nech je na neho relativita robit
 # Centered na playera aj s cords, nech je on 0,0 vzdy vlastne takze no nevem
+# ked stupne na nejaku platformu sa posunie minimal height co je vlastne podla coho sa to bude spravat
+
+from tkinter import *
 
 root = Tk()
 root.geometry("1280x720")
@@ -14,17 +15,21 @@ canvas.pack()
 
 dosky = []
 key = ""
+gravity = True
+
 
 class Player:
-    def __init__(self, master=None):
+    def __init__(self):
         self.x = 0
         self.y = 0
 
-        self.rectangle = canvas.create_rectangle(5, 5, 25, 25, fill="black")
+        self.rectangle = canvas.create_rectangle(5, 5, 25, 25, fill="black", outline="red")
         canvas.pack()
+        self.pos = canvas.coords(self.rectangle)
 
     def movement(self):
         canvas.move(self.rectangle, self.x, self.y)
+        self.pos = canvas.coords(self.rectangle)
 
     def left(self):
         self.x = -5
@@ -42,8 +47,9 @@ class Player:
         self.movement()
 
     def down(self):
+        print("down")
         self.x = 0
-        self.y = 5
+        self.y = 1
         self.movement()
 
     def keypressed(self):
@@ -64,48 +70,48 @@ class Player:
 
 
 class Doska:
-    def __init__(self, x1, y1, x2, y2):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
+    def __init__(self):
+        self.y = 0
+        self.x = 0
+        self.rectangle = canvas.create_rectangle(105, 105, 175, 115, fill="black")
+        self.pos = canvas.coords(self.rectangle)
+        canvas.pack()
+
+    def movement(self):
+        canvas.move(self.rectangle, self.x, self.y)
+
+    def up(self):
+        self.x = 0
+        self.y = -5
+        self.movement()
+
+    def down(self):
+        self.x = 0
+        self.y = 5
+        self.movement()
 
 
 player = Player()
-doska0 = Doska(0, 0, 0, 0)
+doska0 = Doska()
 dosky.append(doska0)
-doska1 = Doska(0, 0, 0, 0)
+doska1 = Doska()
 dosky.append(doska1)
-doska2 = Doska(0, 0, 0, 0)
+doska2 = Doska()
 dosky.append(doska2)
-doska3 = Doska(0, 0, 0, 0)
+doska3 = Doska()
 dosky.append(doska3)
-doska4 = Doska(0, 0, 0, 0)
+doska4 = Doska()
 dosky.append(doska4)
-doska5 = Doska(0, 0, 0, 0)
+doska5 = Doska()
 dosky.append(doska5)
-doska6 = Doska(0, 0, 0, 0)
+doska6 = Doska()
 dosky.append(doska6)
-doska7 = Doska(0, 0, 0, 0)
+doska7 = Doska()
 dosky.append(doska7)
-doska8 = Doska(0, 0, 0, 0)
+doska8 = Doska()
 dosky.append(doska8)
-doska9 = Doska(0, 0, 0, 0)
+doska9 = Doska()
 dosky.append(doska9)
-
-for i in range(len(dosky)):
-    print(f"i = {i}")
-    canvas.create_rectangle(dosky[i].x1, dosky[i].y1, dosky[i].x2, dosky[i].y2)
-    print("vykreslene")
-    canvas.update()
-
-
-def jumped():
-    for i in range(len(dosky)):
-        print(f"i = {i}")
-        canvas.move(dosky[i])
-        print("vykreslene")
-        canvas.update()
 
 
 def keypress(arg):
@@ -115,5 +121,19 @@ def keypress(arg):
     player.keypressed()
 
 
+def cycle():
+    global gravity, dosky
+    for i in dosky:
+        if i.pos[0] <= player.pos[2] and i.pos[2] >= player.pos[0] and i.pos[1] <= player.pos[1] + 21 and i.pos[3] <= \
+                player.pos[3] + 21:
+            gravity = False
+            print("satisfies")
+    if gravity == True:
+        player.down()
+    gravity = True
+    canvas.after(100, cycle)
+
+
 root.bind("<Key>", keypress)
+cycle()
 canvas.mainloop()
