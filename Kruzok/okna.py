ab7e9c018fb0d9e7639806cfc408e7bc36387a87
matus.capo <matus.capo11@gmail.com>
2024-02-09 09:14:54 +0100

    Trochu matika programming, chemia learning time

--- Kruzok/okna.py
+++ Kruzok/okna.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from random import *
 
+# je to funkcne ale nastal problem s coordinates, nejak to vymysliet treba
+
 ww = 1280
 wh = 720
 root = Tk()
@@ -24,30 +26,31 @@ for i in x:
 
 def klik(pos):
     print(f"{pos.x}x{pos.y}")
+    print(f"{x}x{y}")
     for i in x:
         for ii in y:
-            if wo + int(str(i)[1]) * size < pos.x < wo + int(str(i)[1]) * size + width and ho + int(str(ii)[1]) * size < pos.y < ho + int(str(ii)[1]) * size + width:
+            if wo + int(str(i)[0]) * size < pos.x < wo + int(str(i)[0]) * size + width and ho + int(str(ii)[0]) * size < pos.y < ho + int(str(ii)[0]) * size + width:
                 print(f"{i}x{ii}")
-                if int(str(i)[1]) == 1 and int(str(ii)[1]) == 1:
-                    canvas.create_rectangle(wo + i * size, ho + ii * size, wo + i * size + width,
-                                            ho + ii * size + width,
-                                            fill="white")
+                print(f"{int(str(i)[1])}s{int(str(ii)[1])}")
+                if int(str(i)[1]) == 1 or int(str(ii)[1]) == 1:
+                    canvas.create_rectangle(wo + int(str(i)[0]) * size, ho + int(str(ii)[0]) * size,
+                                wo + int(str(i)[0]) * size + width, ho + int(str(ii)[0]) * size + width)
                     for a in range(len(x)):
                         if i == x[a]:
-                            x[a] = int(str(i)[1]) - 1
+                            x[a] = x[a] - 1
                     for a in range(len(y)):
                         if ii == y[a]:
-                            y[a] = int(str(ii)[1]) - 1
-                elif int(str(i)[1]) == 0 and int(str(ii)[1]) == 0:
-                    canvas.create_rectangle(wo + i * size, ho + ii * size, wo + i * size + width,
-                                            ho + ii * size + width,
+                            y[a] = y[a] - 1
+                if int(str(i)[1]) == 0 or int(str(ii)[1]) == 0:
+                    canvas.create_rectangle(wo + int(str(i)[0]) * size, ho + int(str(ii)[0]) * size,
+                                wo + int(str(i)[0]) * size + width, ho + int(str(ii)[0]) * size + width,
                                             fill="pink")
                     for a in range(len(x)):
                         if i == x[a]:
-                            x[a] = int(str(i)[1]) + 1
+                            x[a] = x[a] + 1
                     for a in range(len(y)):
                         if ii == y[a]:
-                            y[a] = int(str(ii)[1]) + 1
+                            y[a] = y[a] + 1
                 print(f"{x}x{y}")
 
 
